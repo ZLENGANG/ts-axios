bee4edf3d9451b4794b11f22f3d23cabba3915cf
ZLENGANG <1437004632@qq.com>
2021-02-23 00:34:16 +0800

    新增简单的请求以及调用方法

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { type } from "os";
+
+export type Method = 'get' | 'GET'
+    | 'delete' | 'DELETE'
+    | 'head' | 'HEAD'
+    | 'options' | 'OPTIONS'
+    | 'post' | 'POST'
+    | 'put' | 'PUT'
+    | 'patch' | 'PATCH'
+export interface AxiosRequestConfig {
+    url: string,
+    method?: Method,
+    data?: any,
+    params?: any
+}
